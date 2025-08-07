9c975beb8513ee40b457840cea68a1f8c8ec3f3f
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-08-07 17:07:17 -0300

    Punto 4 realizado

--- src/main/java/plantilla/EjercicioPOO.java
+++ src/main/java/plantilla/EjercicioPOO.java
@@ -3,9 +3,7 @@ package plantilla;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-
-
+import java.util.Scanner;
 /**
  *
  * @author Marcelo
@@ -40,20 +38,20 @@ public class EjercicioPoo {
 
     public static void main(String[] args) {
         //Estructura de datos con herencia y polimorfimo
-        List<Ticket> listaTickets = new ArrayList<Ticket>();
+        List <Ticket> listaTickets = new ArrayList <Ticket> ();
 
         //creacion de ticket mediante parametros
         Ticket ticket1 = new Ticket(1, 1, 2, 6500, new Date(), new Date());
         Ticket ticket2 = new Ticket(1, 1, 1, 6500, new Date(), new Date());
 
         //creacion de ticket mediante construtor vacio y aplico encapsulamiento
-        Ticket ticket3 = new Ticket();
-        ticket3.setNumero(3);
-        ticket3.setFila(3);
-        ticket3.setAsiento(1);
-        ticket3.setPrecio(8000);
-        ticket3.setFechaCompra(new Date());
-        ticket3.setFechaValidez(new Date());
+        Ticket ticket3=new Ticket();
+            ticket3.setNumero(3);
+            ticket3.setFila(3);
+            ticket3.setAsiento(1);
+            ticket3.setPrecio(8000);
+            ticket3.setFechaCompra(new Date());
+            ticket3.setFechaValidez(new Date());
 
         //agrego los elementos a la lista
         listaTickets.add(ticket1);
@@ -63,10 +61,39 @@ public class EjercicioPoo {
         //PUNTO 3: Suma de precios de todos los tickets:
 
         double suma = 0;
-        for(Ticket tick: listaTickets){
+        for (Ticket tick: listaTickets) {
             suma += tick.getPrecio();
         }
 
         System.out.println("La suma de precio de todos los tickets es: " + suma);
+
+        //Punto 4:
+        //pedir fila al usuario
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese la fila que desee consultar:");
+        System.out.println("-------------------------------------");
+        int fila = teclado.nextInt();
+        boolean condicion = false;
+
+
+        for (Ticket tick: listaTickets ){
+            if(tick.getFila() == fila){
+                System.out.println("\n================ TICKET =================");
+                System.out.println("Numero          : " + tick.getNumero());
+                System.out.println("Fila            : " + tick.getFila());
+                System.out.println("Asiento         : " + tick.getAsiento());
+                System.out.println("Precio          : $" + tick.getPrecio());
+                System.out.println("Fecha de compra : " + tick.getFechaCompra());
+                System.out.println("Validez hasta   : " + tick.getFechaValidez());
+                System.out.println("=========================================\n");
+                condicion = true;
+            }
+        }
+        if(condicion == false){
+            System.out.println("Ticket no encontrado en la fila: " + fila);
+        }
     }
 }
+
+//Para compilar todos los archivos: javac src/main/java/plantilla/*.java
+//Ejecutar la clase especificando carpeta como classpath: java -cp src/main/java plantilla.EjercicioPoo
